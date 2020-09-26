9464b75fc4c1384ab922d9ab591d9ec5ac9097ba
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-26 12:52:45 +0300

    initial migration made

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-login_manager.login_view = 'autth.login'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
   app =Flask(__name__)
@@ -24,5 +24,7 @@ def create_app(config_name):
   #Register Bluepriint
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
   
   return app
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY=ebay
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ebay:qwerty@localhost/blog'
 
 
 python3.8 manage.py server
