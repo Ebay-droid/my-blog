b324a445c581f8c1e11729562092b59d4a13ff92
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-26 13:13:26 +0300

    index file added

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,10 @@
-from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from flask_login import login_required,current_user
+from  ..models import User
+from .. import db
+
+@main.route('/')
+def index():
+  
+  return render_template('index.html')
