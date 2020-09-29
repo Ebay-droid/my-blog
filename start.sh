70d83fcda982204e8429d559e5bb1bae1416413a
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-29 13:57:22 +0300

    push for deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export SECRET_KEY=ebay
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ebay:qwerty@localhost/my_blog'
+# export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ebay:qwerty@localhost/my_blog'
 export QUOTES_API='http://quotes.stormconsultancy.co.uk/random.json'
-export DATABASE_URL='postgres://bamuzfjwhgqruz:2c109c1f967d4bf3f1fb54bbb6caabca002b1c9293a763abaeb4d54acc35f17b@ec2-54-165-164-38.compute-1.amazonaws.com:5432/ddc3n69j5ifiat'
-
+export DATABASE_URL='postgres://bkyexjhelifpxq:d859ee28474c5f172eacb6064a4c443e7699ad25abebac86616f0077731050cd@ec2-34-233-43-35.compute-1.amazonaws.com:5432/dfsp776liv0nvu'
 python3.8 manage.py server
