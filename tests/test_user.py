31c2530d3d0c2e508ec460ec5403da73cb1beee6
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-30 10:17:19 +0300

    gitignore update

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,12 @@ from app.models import User
 class  UserModelTest(unittest.TestCase):
   
   def setUp(self):
-    self.new_user = User(password = 'qwerty')
+    self.new_user = User(username='ebay',email='ben@gmail.com',password = 'qwerty')
+    
+  def test_check_instance(self):
+    self.assertEquals(self.new_user.username, 'ebay')
+    self.assertEquals(self.new_user.email,'ben@gmail.com')
+    self.assertEquals(self.new_user.pass_secure,'qwerty')  
     
   def test_password_setter(self):
     self.assertTrue(self.new_user.pass_secure is not  None)
